0087846189ea97f902735399e1b8666a34ea9240
Arnold Chand <creativenull@outlook.com>
2020-12-07 21:18:43 -0400

    update to working commands

--- bot.py
+++ bot.py
@@ -1,40 +1,23 @@
 import os
-from twitchio.enums import WebhookMode
 from twitchio.ext import commands
-from twitchio.webhook import UserFollows
 
-class CNullBot(commands.Bot):
-    def __init__(self):
-        super().__init__(
-            irc_token=os.environ['TMI_TOKEN'],
-            client_id=os.environ['CLIENT_ID'],
-            nick=os.environ['BOT_NICK'],
-            prefix=os.environ['BOT_PREFIX'],
-            initial_channels=[os.environ['CHANNEL']],
-            webhook_server=True,
-            local_host='localhost',
-            port=8080
-        )
+bot = commands.Bot(
+    irc_token=os.environ['TMI_TOKEN'],
+    api_token=os.environ['OAUTH_TOKEN'],
+    client_id=os.environ['CLIENT_ID'],
+    nick=os.environ['BOT_NICK'],
+    prefix=os.environ['BOT_PREFIX'],
+    initial_channels=['#creativenull'],
+)
 
-    async def event_ready(self):
-        print(f"{self.nick} is alive!")
+@bot.event
+async def event_ready():
+    print(f"{bot.nick} is live")
 
-        // TODO
-        await self.modify_webhook_subscription(
-            callback='localhost',
-            mode=WebhookMode.subscribe,
-            topic=UserFollows(first=1, to_id=53017100),
-            lease_seconds=0,
-            secret=os.environ['SECRET_ID']
-        )
-
-    async def event_webhook(self, data):
-        print(data)
-
-    @commands.command(name="test")
-    async def test_cmd(self, ctx):
-        await ctx.send(f"/me This is a test")
-
+@bot.command(name='hello', aliases=['hey', 'hi'])
+async def hello(c):
+    message = f"/me no u"
+    await c.send(message)
 
 if __name__ == "__main__":
-    CNullBot().run()
+    bot.run()
