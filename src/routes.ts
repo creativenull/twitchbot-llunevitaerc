f330d0eddef9abbd847eaed26e5a846532ccd408
Arnold Chand <creativenull@outlook.com>
2022-03-01 21:56:18 -0400

    feat(routes): add command routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "https://deno.land/x/oak@v10.1.0/mod.ts";
 import { allQuestions } from "./repositories/question.ts";
 import { allUsers, readUser } from "./repositories/user.ts";
+import { addCommand, getCommands } from "./repositories/command.ts";
+import type { Command } from "./repositories/command.ts";
 
 const router = new Router();
 
@@ -25,6 +27,29 @@ router.get("/users/:id", (ctx) => {
   } catch (_) {
     ctx.response.status = 404;
     ctx.response.body = null;
+  }
+});
+
+router.get("/commands", (ctx) => {
+  try {
+    ctx.response.body = getCommands();
+  } catch (e) {
+    ctx.response.status = 404;
+    ctx.response.body = null;
+  }
+});
+
+router.post("/commands/add", async (ctx) => {
+  try {
+    const req = ctx.request.body();
+    if (req.type === "json") {
+      const data = await req.value as Command;
+      addCommand(data);
+      ctx.response.body = { status: "success" };
+    }
+  } catch (e) {
+    ctx.response.status = 400;
+    ctx.response.body = { status: "failed", data: e.message };
   }
 });
 
